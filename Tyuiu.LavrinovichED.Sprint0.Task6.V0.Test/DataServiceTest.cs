16471eef39b2ab4c28b2e627b10b269e7f1f9386
Tyuiu.lavrinoviched@outlook.com <Tyuiu.lavrinoviched@outlook.com>
2024-09-16 22:48:36 +0500

    Task 6

---
+++ Tyuiu.LavrinovichED.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+using Tyuiu.LavrinovichED.Sprint0.Task6.V0.Lib; 
+
+namespace Tyuiu.LavrinovichED.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 }; 
+            var res = DataService.AdditionArray(numbers); 
+            Assert.AreEqual(15, res); 
+        }
+
+        [TestMethod]
+        public void CheckAdditionArrayValid2()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+
+        [TestMethod]
+        public void CheckAdditionArrayValid3()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
